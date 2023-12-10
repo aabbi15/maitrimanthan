f0304165b0819343015e5edcc31cfa2a3dbc0d8d
Abhishek Abbi <abhishekabbi.work@gmail.com>
2023-12-10 15:28:33 +0530

    footer

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,14 @@
+import Footer from "./components/footer";
 import Navbar, { ComplexNavbar } from "./components/header";
 
 export default function App(){
   return(
     <div>
       <Navbar/>
+
+      <div className="h-screen bg-slate-100"></div>
+
+      <Footer/>
     </div>
     
   )
