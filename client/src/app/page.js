0c8062f369da8548a711be31f97bcd3eb819c0e4
Anonymous <swapnilshukla470@gmail.com>
2023-12-12 19:25:28 +0530

    k

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -14,9 +14,9 @@ export default function Home ()
 
     <div class="flex flex-col lg:flex-row justify-between space-x-20">
         <div class="text-center lg:text-left mt-40">
-            <h1 class="font-semibold text-white-900 text-3xl md:text-6xl leading-normal mb-6">Charity for the <br/> world better life</h1>
+            <h1 class="font-semibold text-white-900 text-3xl md:text-5xl leading-normal mb-5"> Intern with purpose, <br/> shape tomorrow.</h1>
 
-            <p class="font-light text-gray-400 text-md md:text-lg leading-normal mb-12">We provide a trusted donation channel for peoples of <br/> worldwide to support people and organizers</p>
+            <p class="font-light text-gray-400 text-md md:text-lg leading-normal mb-12">We provide Internships for peoples of <br/> nationwide to support people who are in need.</p>
 
             <button class="px-6 py-4 bg-info font-semibold text-white text-lg rounded-xl hover:bg-blue-700 transition ease-in-out duration-500">Get started</button>
         </div>
