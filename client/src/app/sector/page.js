8f670bac2f6716979beb3f219dd8cf404758b645
Abhishek Abbi <abhishekabbi.work@gmail.com>
2023-12-12 17:46:54 +0530

    push

---
+++ client/src/app/sector/page.js
@@ -0,0 +1,21 @@
+
+import Card from "../components/cards";
+import Footer from "../components/footer";
+import Navbar, { ComplexNavbar } from "../components/header";
+
+export default function App(){
+  return(
+    <div>
+      <Navbar/>
+
+      <div className="min-h-screen px-40 pt-24 bg-slate-300">
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+
+      <Footer/>
+    </div>
+    
+  )
+}
