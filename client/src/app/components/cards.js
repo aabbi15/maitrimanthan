ead3c77ff5a9592219469c62e653f7649eb09e31
Anonymous <swapnilshukla470@gmail.com>
2023-12-16 12:44:20 +0530

    days

--- client/src/app/components/cards.js
+++ client/src/app/components/cards.js
@@ -6,7 +6,7 @@ export default function Card1(){
         <div className="pb-10">
             
 
-<a href="#" class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:w-full hover:bg-gray-900 justify-center ">
+<a href="http://localhost:3000/days/healthcare" class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:w-full hover:bg-gray-900 justify-center ">
 
         <div class="flex flex-col justify-between p-4 leading-normal ">
         <Image
